ac0e1a9419a0a2f7c2da3ac7bd66b466a1b4c4b2
ioiein <71278916+ioiein@users.noreply.github.com>
2021-04-23 22:16:39 +0300

    added pretrained weights

--- contest01/model.py
+++ contest01/model.py
@@ -49,10 +49,10 @@ class SEModule(nn.Module):
 class RESNEXT_steroid(nn.Module):
     def __init__(self):
         super(RESNEXT_steroid, self).__init__()
-        model = models.resnext50_32x4d()
+        model = models.resnext50_32x4d(pretrained=True)
         model.fc = nn.Linear(model.fc.in_features, 2 * NUM_PTS, bias=True)
-        checkpoint = torch.load("./runs/baseline_full4_best.pth", map_location='cpu')
-        model.load_state_dict(checkpoint, strict=True)
+        #checkpoint = torch.load("./runs/baseline_full4_best.pth", map_location='cpu')
+        #model.load_state_dict(checkpoint, strict=True)
         self.base_net = nn.Sequential(*list(model.children())[:-2])
         out_size = model.fc.in_features
         self.linear7 = ConvBlock(out_size, out_size, (4, 4), 1, 0, dw=True, linear=True) #(7x7)
