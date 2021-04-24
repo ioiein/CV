278468fe45a6c7307f54089e834331f2279f76b4
ioiein <71278916+ioiein@users.noreply.github.com>
2021-04-25 00:20:43 +0300

    1

--- contest01/model.py
+++ contest01/model.py
@@ -47,10 +47,10 @@ class SEModule(nn.Module):
         return input * x
 
 
-class RESNET_st(nn.Module):
+class RESNEXT_st(nn.Module):
     def __init__(self):
-        super(RESNET_st, self).__init__()
-        model = models.resnet18()
+        super(RESNEXT_st, self).__init__()
+        model = models.resnext50_32x4d()
         model.fc = nn.Linear(model.fc.in_features, 2 * NUM_PTS, bias=True)
         #checkpoint = torch.load("./runs/baseline_full4_best.pth", map_location='cpu')
         #model.load_state_dict(checkpoint, strict=True)
@@ -70,3 +70,79 @@ class RESNET_st(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+
+class AvgResNet(nn.Module):
+    def __init__(self, output_size=2*NUM_PTS):
+        super(AvgResNet, self).__init__()
+        self.model = models.resnet34(pretrained=True)
+        self.model.requires_grad_(True)
+
+        fc_input = 512 * (CROP_SIZE // 32) ** 2
+        s1_input = 256 * 4 ** 2
+        s2_input = 128 * 8 ** 2
+        self.model.fc = nn.Linear(fc_input + s1_input + s2_input, output_size, bias=True)
+        self.model.fc.requires_grad_(True)
+        self.model.layer4.requires_grad_(True)
+        self.model.layer3.requires_grad_(True)
+        self.model.layer2.requires_grad_(True)
+        self.avg_pool1 = nn.AdaptiveAvgPool2d((4, 4))
+        self.avg_pool2 = nn.AdaptiveAvgPool2d((8, 8))
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x1 = self.model.layer1(x)
+        x2 = self.model.layer2(x1)
+        x3 = self.model.layer3(x2)
+        x4 = self.model.layer4(x3)
+
+        x4 = torch.flatten(x4, 1)
+        x3 = self.avg_pool1(x3)
+        x3 = torch.flatten(x3, 1)
+        x2 = self.avg_pool2(x2)
+        x2 = torch.flatten(x2, 1)
+
+        out = torch.cat([x4, x3, x2], 1)
+        out = self.model.fc(out)
+        return out
+
+
+class CnnResNet(nn.Module):
+    def __init__(self, output_size=2*NUM_PTS):
+        super(CnnResNet, self).__init__()
+        self.model = models.resnet34(pretrained=True)
+        self.model.requires_grad_(True)
+
+        fc_input = 512 * 4 ** 2
+        s1_input = 64 * 8 ** 2
+        s2_input = 32 * 16 ** 2
+        self.model.fc = nn.Linear(fc_input + s1_input + s2_input, output_size, bias=True)
+        self.model.fc.requires_grad_(True)
+        # self.model.layer4.requires_grad_(True)
+        # self.model.layer3.requires_grad_(True)
+        # self.model.layer2.requires_grad_(True)
+        self.x3_bottleneck = nn.Conv2d(256, 64, (1, 1))
+        self.x4_bottleneck = nn.Conv2d(128, 32, (1, 1))
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x1 = self.model.layer1(x)
+        x2 = self.model.layer2(x1)
+        x3 = self.model.layer3(x2)
+        x4 = self.model.layer4(x3)
+
+        x4 = torch.flatten(x4, 1)
+        x3 = self.x3_bottleneck(x3)
+        x3 = torch.flatten(x3, 1)
+        x2 = self.x4_bottleneck(x2)
+        x2 = torch.flatten(x2, 1)
+        out = torch.cat([x4, x3, x2], 1)
+        out = self.model.fc(out)
+        return out
