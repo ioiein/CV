4f20f3519721de69ae029b1c0fa21171e2d2cec4
ioiein <71278916+ioiein@users.noreply.github.com>
2021-04-25 22:28:57 +0300

    fix min lr

--- contest01/train.py
+++ contest01/train.py
@@ -165,7 +165,7 @@ def main(args):
     #    threshold_mode='abs', cooldown=0,
     #    min_lr=1e-6, eps=1e-08
     #)
-    lr_scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.learning_rate)
+    lr_scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.learning_rate, final_div_factor=1e-6)
 
     # 2. train & validate
     print("Ready for training...")
